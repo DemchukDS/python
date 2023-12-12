bb7c15bb442a839556c93ef72e11b376ea0f8fa7
Dmitriy Demchuk <demchuk.btg@gmail.com>
2023-12-12 15:45:52 +0300

    Ввод чисел с консоли

--- py_lecture001/lecture001.py
+++ py_lecture001/lecture001.py
@@ -2,7 +2,12 @@ n = 'Demchuk\'Dima'
 a = 6
 b = 1.23
 
+print("Enter the first number")
+j = input()
+
+k = input('Enter the secoond number: ')
+
 print(f"{n} - {a} - {b}")
 
 
-print("{} - {} - {}".format(n, a, b))
+print("{} - {} - {} - {} - {}".format(n, a, b, j, k))
