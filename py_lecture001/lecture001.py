cb3f719d5382eaaae829b2745faefaae2d6fd0c1
Dmitriy Demchuk <demchuk.btg@gmail.com>
2023-12-14 14:18:56 +0300

    update

--- py_lecture001/lecture001.py
+++ py_lecture001/lecture001.py
@@ -1,21 +1,5 @@
-numberOne = input("Enter the first number: ")
-numberTwo = input("Enter the second number: ")
+numberOne = float(input("Enter the first number: "))
+numberTwo = float(input("Enter the second number: "))
 
-sum = int(numberOne) + int(numberTwo)
-
-print(sum)
-
-
-n = 8.12
-print(n)
-print(type(n))
-k = 7
-print(k)
-print(type(k))
-t = 'Demchuk'
-print(t)
-print(type(t))
-
-print(float(n)+float(k))
-print(type(float(n)+float(k)))
-print(str(n)+str(k)+str(t))
+result = round(numberOne * numberTwo, 2)
+print(result)
